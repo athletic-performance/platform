8f48f1ce41bb76bec16adf9f171f73c7b830a311
Igor Manin <106236581+IgorManin@users.noreply.github.com>
2026-09-16 22:51:22 +0300

    test: add staging backend error endpoint (#62)

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -9,6 +9,7 @@ import { loadApiEnv } from './config/env';
 import { HealthModule } from './health/health.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { VersionModule } from './version/version.module';
+import { TestModule } from './test-endpoint/test.module';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { VersionModule } from './version/version.module';
     PrismaModule,
     HealthModule,
     VersionModule,
+    TestModule,
   ],
   providers: [
     {
